40058fb4f068f49723a8d7146208c0dc9ad56614
Heba Alnouri <73322341+heba-alnouri@users.noreply.github.com>
2021-05-10 07:37:34 +0300

    Start separating some methods from JDBCConnection to classes.

--- src/com/company/JDBCConnection.java
+++ src/com/company/JDBCConnection.java
@@ -4,58 +4,58 @@ import java.sql.*;
 
 public class JDBCConnection {
 
-    private static Connection connection;
-    private static Statement statement;
-    private static ResultSet resultSet;
+    private  Connection connection;
+    private  Statement statement;
+    private  ResultSet resultSet;
+    private  PreparedStatement preparedStatement;
 
-
-    public static void main(String[] args) throws SQLException {
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/onlinebookstore", "root", "Nero8board1029");
-        statement = connection.createStatement();
-    }
-
-    // check the validation of the login information.
-    public static boolean validInfoForLogin(String infoType,String info){
-        try {
-            resultSet = statement.executeQuery("select * from user");
-
-            while (resultSet.next()) {
-                if(info.equals(resultSet.getString(infoType))){
-                    return true;
-                }
-            }
+    public JDBCConnection() {
+        try{
+            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/onlinebookstore", "root", "Nero8board1029");
+            this.statement = connection.createStatement();
         }catch (SQLException e){
             e.printStackTrace();
         }
-        return false;
     }
 
-    // add new user.
-    public static void registerInfo(int userId ,String firstName,String lastName,String email,String password,String phoneNumber,String userType,int locationID){
-        try{
-            //statement.executeUpdate("insert into genre (genre_id,type) values (14,'Manga')");
-
-            statement.executeUpdate("insert into user (user_id,first_name,last_name,email,password,phone_number,user_type,location_id)"+
-                    " values ("+userId+",'"+firstName+"','"+lastName+"','"+email+"','"+password+"','"+phoneNumber+"','"+userType+"','"+locationID+"')");
-
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+    public Connection getConnection() {
+        return connection;
     }
 
-    // get information for a new location and insert them to the database in the location table.
-    public static void locationInfo(int locationId, String country, String state, String city, String address){
-        try{
-            statement.executeUpdate("insert into location (location_id,country,state,city,address)"+
-                    " values ("+locationId+",'"+country+"','"+state+"','"+city+"','"+address+"')");
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
 
-        } catch (SQLException e){
-            e.printStackTrace();
-        }
+    public Statement getStatement() {
+        return statement;
+    }
+
+    public void setStatement(Statement statement) {
+        this.statement = statement;
+    }
+
+    public ResultSet getResultSet() {
+        return resultSet;
+    }
+
+    public PreparedStatement getPreparedStatement() {
+        return preparedStatement;
+    }
+
+    public void setPreparedStatement(PreparedStatement preparedStatement) {
+        this.preparedStatement = preparedStatement;
+    }
+
+    public void setResultSet(ResultSet resultSet) {
+        this.resultSet = resultSet;
+    }
+
+    public static void main(String[] args) throws SQLException {
+
     }
 
     // get information for a new book and insert them to the database in the book table.
-    public static void addBook(int bookId, int publisherId, int authorId, int genre_id, String title, int price, int stocks, int sellings){
+    public void addBook(int bookId, int publisherId, int authorId, int genre_id, String title, int price, int stocks, int sellings){
         try{
             //statement.executeUpdate("insert into genre (genre_id,type) values (14,'Manga')");
 
@@ -68,7 +68,7 @@ public class JDBCConnection {
     }
 
     // function to remove any column from any table. ex(remove book, remove author, remove user, etc).
-    public static void removeColumnInAnyTable(String table, String infoType,String info){
+    public void removeColumnInAnyTable(String table, String infoType,String info){
         try{
             statement.executeUpdate("DELETE FROM `onlinebookstore`.`"+table+"` WHERE (`" + infoType +"` = '"+ info +"')");
 
@@ -77,8 +77,8 @@ public class JDBCConnection {
         }
     }
 
-    //get one attribute from any table with different conditions.
-    public static void getAnyAttributeFromAnyTable(String table, String findBy, String infoType,String info){
+    // get one attribute from any table with different conditions.
+    public void getAnyAttributeFromAnyTable(String table, String findBy, String infoType,String info){
         try {
             resultSet = statement.executeQuery("select "+infoType+" from "+table+" WHERE "+findBy+" = "+info);
             //resultSet = statement.executeQuery("SELECT * FROM genre WHERE genre_id = 1");
@@ -90,10 +90,13 @@ public class JDBCConnection {
         }
     }
 
-
-
-
-
+    public  void closeConnection(){
+        try{
+            connection.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
 
 
 }
